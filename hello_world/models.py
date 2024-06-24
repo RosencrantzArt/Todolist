7a370b76357d52bf908d9811fefaedee40957508
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-06-24 13:49:06 +0000

    add postmodel

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)  # Optional field for additional information
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # Adjust as needed
+    status = models.CharField(max_length=20, choices=[('P', 'Pending'), ('C', 'Completed')], default='P')
